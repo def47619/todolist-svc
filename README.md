fb84abf9d37e77a07906297b81d623448aca0558
def47619 <def47619@gmail.com>
2023-04-25 05:16:34 +0900

    feat: 230425 - express 사용
    - express 설치 -> nodemon 인식 안됨 -> nodemon 재설치
    - server.js:
    	- header 작성하지 않으려고 json 메서드 사용, 쓰면 오히려 에러남
    	- 페이지에 따라 다른 데이터 반환

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # todolist-svc
 
 ### npm packages
+- npm install --save express
 - npm install -D nodemon 
     - 변경된 사항 모니터링 -> 테스트 중인 서버에 바로 적용 
+
+
+## API sites
+- https://contactsvc.bmaster.kro.kr/contacts?pageno=1$pagesize=4
+- https://contactsvc.bmaster.kro.kr/contacts
+
+
+## 테스트 프로그램 
+- postman : 네트워크 테스트 
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-const http = require('http');
-const PORT = 8080;
+const express = require('express');
+const app = express();
 
- // 서버를 구동할 함수를 설정 
-const server = http.createServer(async (req, res) => { // req : request / res : response
-    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' }); // 한글 깨짐 방지 위함 
-    res.write("<h1>나 정도면 잘생김</h1>");
-    res.write("<h2>나 정도면 잘생김</h2>");
-    res.end(`<p>${req.url}</p>`);
-}).listen(PORT);
+app.get('/', (req, res) => {
+    // 헤더 없어도 됨, 헤더 안 쓰려고 json 메서드 쓰는 것임 
+    // res.writeHead(200, { 'Content-Type': 'application/json' }); 
+    res.json({requrl: req.url, msg:'hello world'});
+});
 
-server.on('listening', () => {
-    console.log(`${PORT} 포트에서 서버가 시작되었습니다.`);
+app.get('/ko', (req, res) => {
+    // res.writeHead(200, { 'Content-Type': 'application/json' }); // 한글 깨짐 방지 위함 
+    res.json({requrl: req.url, msg:'안녕'});
+});
+
+app.listen(8080, () => {
+    console.log(`### 8080 포트의 서버가 구동되었습니다.`);
 });
