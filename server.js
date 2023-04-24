469f8ba62991836f3a92e05407eb14ed404e6ecf
def47619 <def47619@gmail.com>
2023-04-25 05:27:44 +0900

    feat: html 전송 및 직접 logger 함수 제작
    - json 함수가 아니라 sendFile 함수로, 해당 웹 페이지에 대한 응답으로
    	- index.html 파일을 전송
    - logger 작성하고 express 앱이 use 함수를 통해 모든 함수에서 logger 사용

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
 
+const logger = (req, res, next) => {
+    console.log(`### ${req.method}, ${req.url}`);
+    next();
+};
+
+app.use(logger); // 순서가 중요하다. 맨 처음에 넣고, 해당 로거 함수 내에 next 있어야 다음 함수로 넘어간다. 
+// next를 하면, 밑의 함수 코드로 넘어가는 것을 허용하는 것이라 봐도 된다. 
+
 app.get('/', (req, res) => {
-    // 헤더 없어도 됨, 헤더 안 쓰려고 json 메서드 쓰는 것임 
-    // res.writeHead(200, { 'Content-Type': 'application/json' }); 
-    res.json({requrl: req.url, msg:'hello world'});
+    res.sendFile(__dirname + '/index.html'); // 해당 응답 결과로 index.html을 보내는 함수
 });
 
 app.get('/ko', (req, res) => {
+    // 헤더 없어도 됨, 헤더 안 쓰려고 json 메서드 쓰는 것임 
     // res.writeHead(200, { 'Content-Type': 'application/json' }); // 한글 깨짐 방지 위함 
     res.json({requrl: req.url, msg:'안녕'});
 });
