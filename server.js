37f697cc0e49109963cb52d58002d1bd24aa4ad2
def47619 <def47619@gmail.com>
2023-04-25 22:39:19 +0900

    - feat: 230425 - parsing / static indexing
    - express.json():
    	- content-type이 application/json일 때 express.json()으로
    	- payload가 파싱됨
    - express.urlencoded():
    	- content-type이 application/x-www-form-urlencoded이면
    	- express.urlencoded()로 payload가 파싱됨
    - express.static():
    	- 화면에 보여줄 모든 내용물들은 해당 디렉토리 내에서 찾아보라는
    	- 설정값
    	- localhost:8080/intro.html으로 요청해야 한다.

--- server.js
+++ server.js
@@ -9,9 +9,18 @@ const logger = (req, res, next) => {
 app.use(logger); // 순서가 중요하다. 맨 처음에 넣고, 해당 로거 함수 내에 next 있어야 다음 함수로 넘어간다. 
 // next를 하면, 밑의 함수 코드로 넘어가는 것을 허용하는 것이라 봐도 된다. 
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html'); // 해당 응답 결과로 index.html을 보내는 함수
-});
+// content-type이 application/json이면 express.json()으로 payload가 파싱됨
+app.use(express.json()); // 요청 정보를 파싱해주는 역할, json 형식의 요청 정보를 파싱함
+
+// content-type이 application/x-www-form-urlencoded이면 urlencoded로 payload가 파싱됨
+app.use(express.urlencoded({extended: true})); // 요청 정보를 파싱해주는 역할, urlencoded 형식의 요청 정보를 파싱함
+
+app.use(express.static(__dirname + '/public')); 
+// localhost:8080/intro.html 으로 요청해야 한다. 
+
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/index.html'); // 해당 응답 결과로 index.html을 보내는 함수
+// });
 
 app.get('/ko', (req, res) => {
     // 헤더 없어도 됨, 헤더 안 쓰려고 json 메서드 쓰는 것임 
