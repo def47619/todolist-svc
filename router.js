27f92402f67f28b30225fd846042fc4aa21e1490
def47619 <def47619@gmail.com>
2023-04-28 21:20:24 +0900

    - feat: 230428 - router.js fileDB 접근 및 데이터 전송
    - router.get:
    	- url에서 받기 : req.params 객체로 url의 params 받을 수 있음
    	- 사용자 이름에 맞는 전체 데이터 탐색
    - router.post:
    	- url에서 받기 불가능 : req.body에서 받아야 함
    	- post 요청을 보낼 때 json으로 보냄
    		- "todo": "node.js 공부",
    		- "desc": "가열차게!!"
    	- 위의 json 형식을 db에 맞춰 보내야 함
    		- 서버 코드 단을 작성할 때도 알아야 함
    	- todolistDao.addTodo 함수를 통해 데이터 추가
    		- 추가한 내용을 사용자에게도 출력
    	- 일반 url 요청으로는 post 방식 확인 불가
    		- postman 프로그램으로 요청 및 결과 확인

--- router.js
+++ router.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const todolistDao = require('./dao/todolistDao'); // 파일 db 접근하려면 require 코드 필요
 
+// 메인 페이지 접근 
 router.get('/', (req, res) => {
     console.log('### Get /');
     res.render("index", {
@@ -8,5 +10,24 @@ router.get('/', (req, res) => {
         subtitle: "(node.js + express + lokijs)",
     });
 });
+
+// 특정 사용자의 모든 내용 출력 / 데이터 얻기
+router.get('/todolist/:owner', (req, res) => {
+    const owner = req.params.owner; // 데이터 조회
+    let result = todolistDao.getTodoList({owner}); // json 형태의 데이터 반환
+    res.json(result);    
+});
+
+// post : body로 전달됨 + 데이터 추가
+// json이 전달되려면 javascript 객체로 전달되어야 한다.
+router.post('/todolist/:owner', (req, res) => {
+    let {todo, desc} = req.body;
+    const owner = req.params.owner;
+    const result = todolistDao.addTodo({owner, todo, desc}); // post : 데이터 db에 추가
+    res.json(result); // 추가한 데이터를 사용자에게도 출력
+
+    // 우리가 브라우저 url로 post 요청을 할 수 없다.
+    // postman 프로그램으로 post 요청을 수행해야 한다.
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const startServer = async () => {
     const app = express();
 
     app.use(logger);
-    app.use(express.static(BASEDIR + '/public'));
+    app.use(express.static(BASEDIR + '/public')); // /intro.html 요청하면 public/intro.html 반환해줌
     app.set('views', BASEDIR + '/views'); // views라는 폴더를 viewpage로 생성
     app.set('view engine', 'ejs'); // 페이지 엔진은 ejs를 쓸 것이다.
     app.engine('html', require('ejs').renderFile); // html이 아닌 것들은 ejs -> html로 render하는 모듈로 변환하여 보여주겠다.
